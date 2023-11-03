b92a631359d1613edb8cc4c9856a22f4197af61a
Marcin <mcebulski97@gmail.com>
2023-11-03 21:13:08 +0100

    part 1-4 after deletion of old views

--- Project_01/polls/views.py
+++ Project_01/polls/views.py
@@ -23,22 +23,6 @@ class ResultsView(generic.DetailView):
     template_name = "polls/results.html"
     
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
-
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question":question})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question":question})
-
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
