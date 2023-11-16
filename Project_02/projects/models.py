9f8db43220587ea35683704fe9702d0f97478f8e
Marcin <mcebulski97@gmail.com>
2023-11-16 16:38:33 +0100

    Project_02 completed

--- Project_02/projects/models.py
+++ Project_02/projects/models.py
@@ -4,3 +4,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
+    image = models.FileField(upload_to="project_images", blank="True")
+
+    def __str__(self):
+        return self.title
--- Project_02/projects/views.py
+++ Project_02/projects/views.py
@@ -4,9 +4,9 @@ from projects.models import Project
 def project_index(request):
     projects = Project.objects.all()
     context = {"projects":projects}
-    return render(request, "projects/project_index.html", context)
+    return render(request, "projects/index.html", context)
 
 def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
     context = {"project":project}
-    return render(request, "projects/projects_detail.html", context)
+    return render(request, "projects/detail.html", context)
