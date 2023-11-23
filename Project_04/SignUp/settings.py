56a9fc005a24b85484fea3b3f99c4af7d5c7fdec
Marcin <mcebulski97@gmail.com>
2023-11-23 13:17:27 +0100

    fixing automated e-mails using outlook

--- Project_04/SignUp/settings.py
+++ Project_04/SignUp/settings.py
@@ -136,9 +136,9 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = "index"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.protonmail.ch"
+EMAIL_HOST = "smtp.office365.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "sign_up_project@proton.me"
+EMAIL_HOST_USER = "sign_up_project@outlook.com"
 EMAIL_HOST_PASSWORD = "StrictlyConfidential"
