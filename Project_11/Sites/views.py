d984009309906c6ef6e15a99ab2318b3448d50d0
Marcin <mcebulski97@gmail.com>
2024-01-29 13:23:14 +0100

    login system in progress

--- Project_11/Sites/views.py
+++ Project_11/Sites/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib import messages
+from .forms import LoginForm
 
 def index(request):
     return render(request, "base.html", {})
+
+
+def Login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            messages.info(request, f"Welcome {username}")
+            return render(request, "base.html", {"user":user})
+        else:
+            messages.error(request, "Invalid username or password")
+    form = LoginForm
+    return render(request, "Sites/login.html", {"form":form})
