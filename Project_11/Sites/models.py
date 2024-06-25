239478094db4ef79e50050f1c23444e03b583066
Marcin <mcebulski97@gmail.com>
2024-06-25 12:57:34 +0200

    adding UserWall and Post models

--- Project_11/Sites/models.py
+++ Project_11/Sites/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserWall(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="wall")
+
+    def __str__(self):
+        return f"{self.user.username}'s wall"
+    
+class Post(models.Model):
+    wall = models.ForeignKey(UserWall, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.FileField(upload_to="post_images", blank=True)
+
+    def __str__(self):
+        return f"Post on {self.wall.user.username}'s wall"
